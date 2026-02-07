871ff90e6c1a181431516ff4c07380098138ed33
Paul Vincent Noval <noval.paulvincent@gmail.com>
2026-02-07 09:51:18 +0000

    I added Multithreading task: Paul Noval

--- grade_system.py
+++ grade_system.py
@@ -16,3 +16,19 @@ def get_user_input():
     except ValueError:
         print("Invalid input! Using defaults: [85, 90, 78, 92]")
         return [85, 90, 78, 92]
+
+# --- 1. Multithreading Task (Creative: Running Average) ---
+def compute_thread(grade, i):
+    global curr_grades
+    
+    # Simulate I/O delay (reading database, etc.)
+    time.sleep(0.1)
+    
+    # Critical Section: Only one thread updates the shared list at a time
+    with curr_lock:
+        curr_grades.append(grade)
+        # Calculate GWA of all grades processed SO FAR
+        current_gwa = sum(curr_grades) / len(curr_grades)
+        
+        print(f" [Thread-{i}] Processed Grade: {grade} | Running GWA: {current_gwa:.2f}")
+        
